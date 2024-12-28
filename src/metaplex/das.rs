3a632700a424e7320dfd572c2eb8c07517c53b99
dmshvetsov <shvetsovdm@gmail.com>
2024-12-28 23:50:56 +0200

    fix DAS deserialization

--- src/metaplex/das.rs
+++ src/metaplex/das.rs
@@ -12,8 +12,8 @@ pub struct Asset {
 pub struct Content {
     pub metadata: Metadata,
     pub edition_num: Option<u64>,
-    pub files: Vec<AFile>,
-    pub links: Links,
+    pub files: Option<Vec<AssetFile>>,
+    pub links: Option<Links>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -24,23 +24,23 @@ pub struct Authority {
 
 #[derive(Deserialize, Debug)]
 pub struct Links {
-    pub image: String,
-    pub external_url: String,
+    pub image: Option<String>,
+    pub external_url: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct AFile {
+pub struct AssetFile {
     pub uri: String,
     pub mime: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Metadata {
-    pub attributes: Vec<Attribute>,
-    pub description: String,
     pub name: String,
     pub symbol: String,
-    pub token_standard: String,
+    pub attributes: Option<Vec<Attribute>>,
+    pub description: Option<String>,
+    pub token_standard: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -60,3 +60,106 @@ pub struct Compression {
     pub seq: u128,
     pub leaf_id: u128,
 }
+
+// Example of DAS API response
+//
+// {
+//   "jsonrpc": "2.0",
+//   "result": {
+//     "interface": "MplCoreAsset",
+//     "id": "GVXmn44fkeUCMuZexkEqiwBBdv83yucs1LsQKuNC8Yb8",
+//     "content": {
+//       "$schema": "https://schema.metaplex.com/nft1.0.json",
+//       "json_uri": "https://bafybeiad4kztrslpqaclwlnb2ftskpfeqhr3v23myoavpw3omye6xinsjm.ipfs.w3s.link/2479.json",
+//       "files": [
+//         {
+//           "uri": "https://bafybeihzoyqffzzygfmsh3omn5qq6mxhfonhhrrqmxvmm72ifz2336lbve.ipfs.w3s.link/2479.png",
+//           "mime": "image/png"
+//         }
+//       ],
+//       "metadata": {
+//         "attributes": [
+//           {
+//             "value": "Soft Lilac",
+//             "trait_type": "Background"
+//           },
+//           {
+//             "value": "None",
+//             "trait_type": "Clothes"
+//           },
+//           {
+//             "value": "Bucket",
+//             "trait_type": "Head"
+//           },
+//           {
+//             "value": "Cyclops",
+//             "trait_type": "Eye"
+//           },
+//           {
+//             "value": "Java",
+//             "trait_type": "Skin"
+//           },
+//           {
+//             "value": "None",
+//             "trait_type": "Mouth"
+//           }
+//         ],
+//         "description": "3333 Fels, of the AFEL ecosystem where degens, builders, and community unite to create and grow with constant innovation, fueled by the $waa token.",
+//         "name": "FEL #2479",
+//         "symbol": ""
+//       },
+//       "links": {
+//         "image": "https://bafybeihzoyqffzzygfmsh3omn5qq6mxhfonhhrrqmxvmm72ifz2336lbve.ipfs.w3s.link/2479.png"
+//       }
+//     },
+//     "authorities": [
+//       {
+//         "address": "Afbz6gcoYsbuio58RMVfpytmopJPosDLruE5ZAU1srAx",
+//         "scopes": [
+//           "full"
+//         ]
+//       }
+//     ],
+//     "compression": {
+//       "eligible": false,
+//       "compressed": false,
+//       "data_hash": "",
+//       "creator_hash": "",
+//       "asset_hash": "",
+//       "tree": "",
+//       "seq": 0,
+//       "leaf_id": 0
+//     },
+//     "grouping": [
+//       {
+//         "group_key": "collection",
+//         "group_value": "C3JoGupodf1s7JhCKiLNv2hbPRqYkDjxebVmsJh2EtZC"
+//       }
+//     ],
+//     "royalty": {
+//       "royalty_model": "creators",
+//       "target": null,
+//       "percent": 0,
+//       "basis_points": 0,
+//       "primary_sale_happened": false,
+//       "locked": false
+//     },
+//     "creators": [],
+//     "ownership": {
+//       "frozen": false,
+//       "delegated": false,
+//       "delegate": null,
+//       "ownership_model": "single",
+//       "owner": "4DRqQb3ihANJRqHNoenq9gTwp58rVn6jHMg5wiJYsQzf"
+//     },
+//     "supply": null,
+//     "mutable": true,
+//     "burnt": false,
+//     "plugins": {},
+//     "mpl_core_info": {
+//       "plugins_json_version": 1
+//     },
+//     "external_plugins": []
+//   },
+//   "id": 1
+// }
