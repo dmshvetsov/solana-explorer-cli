d0902bfe1e5b81eb9904aaf321d790784d85962f
dmshvetsov <shvetsovdm@gmail.com>
2025-01-11 20:49:00 +0200

    add wait time between tests
    to reduce chance to get rate limited during test runs

--- tests/accounts_test.sh
+++ tests/accounts_test.sh
@@ -2,42 +2,62 @@ set -e
 
 SE=./target/debug/se
 
+# NFTs
+
 echo "\n\npNFT"
 $SE account 5soeS5y4jrsyaXSYpnxjrUT8qsJ3VVNGRtT2jK399gx7
+sleep 0.5
 
 echo "\n\n Metaplex metadata account"
 $SE account FeYYbECgnky3rWrmxBB7cybWgyUNEVKwQQWb8oU8DnMj
+sleep 0.5
 
 echo "\n\np MPL Core NFT"
 $SE account HGFDAByQskKo9Az9yAtgg6qP8TnwMayZGKbyi3vkanzT
+sleep 0.5
 
 echo "\n\np MPL Core NFT with json format"
 $SE account HGFDAByQskKo9Az9yAtgg6qP8TnwMayZGKbyi3vkanzT -f json
+sleep 0.5
+
+echo "\n\ncNFT"
+$SE account 9HNN54hfD3GVy4WkUtXjJdxaTo9tjFzYmEXYN9eHnLZp
+sleep 0.5
+
+# FTs
 
 echo "\n\nBERN token account"
 $SE account 4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R
+sleep 0.5
 
 echo "\n\nBERN token account with short ac alias"
 $SE ac 4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R
+sleep 0.5
+
+# Magic Eden Laucnhpads
 
 # FIXME: failing
 # echo "\n\nCandy Machine Aq9PkNTgWdmhA24xAcKYYZwLDo1CfEUk6EqrZ87RsuEJ"
 # $SE account Aq9PkNTgWdmhA24xAcKYYZwLDo1CfEUk6EqrZ87RsuEJ
+# sleep 0.5
 
 echo "\n\nMagic Eden Core Open Edition"
 $SE account DQebYkR5JxtpCpvNJu1u3FEJDhat3kkVgk3bbBr6iAAW
+sleep 0.5
 
 echo "\n\nMagic Eden Launchpad #1"
 $SE account D2Fbhy8pPi47w7gaDEYmSzskAAQFUqiHG53hUE71HpC2
+sleep 0.5
 
 echo "\n\nMagic Eden Launchpad #2"
 $SE account F1obR8rmTi7a22TE3qRTYsJmeN5YCNgUcuPSwLK8Cooc
+sleep 0.5
 
 echo "\n\nMagic Eden Launchpad #3"
 $SE account HG524wasxJYwHEoxQHdmUn7fryk4YDHFJAHXBHZ2LKCr
+sleep 0.5
 
-echo "\n\ncNFT"
-$SE account 9HNN54hfD3GVy4WkUtXjJdxaTo9tjFzYmEXYN9eHnLZp
+# Wallets
 
 echo "\n\nsmall wallet"
 $SE account J1MdztLMncohKGrfkTASnftzQC3ssKacY9FYQ43Lcowf
--- tests/transactions_test.sh
+++ tests/transactions_test.sh
@@ -4,12 +4,16 @@ SE=./target/debug/se
 
 echo "\n\ntransaction WSOL -> GOAT on Raydium swap"
 $SE transaction 44cfUnsmWHfS1Kz4PcF9Rj4XoLSivfTYPunTQRLnvqAfhBqrYUV2mWoJUXr5goKAUqVp4g4XWBgLJ9rSYva7cxqq
+sleep 0.5
 
 echo "\n\ntransaction WSOL -> GOAT on Raydium swap with short tx alias"
 $SE tx 44cfUnsmWHfS1Kz4PcF9Rj4XoLSivfTYPunTQRLnvqAfhBqrYUV2mWoJUXr5goKAUqVp4g4XWBgLJ9rSYva7cxqq
+sleep 0.5
 
 echo "\n\nlist of account transactions"
 $SE account-transactions HEcFC9JDPR2CrMmUZ6nJpDKfbuFsg26DGYEJWw9B988E
+sleep 0.5
 
 echo "\n\nlist of account transactions with short ac-txs alias"
 $SE ac-txs HEcFC9JDPR2CrMmUZ6nJpDKfbuFsg26DGYEJWw9B988E
+sleep 0.5
