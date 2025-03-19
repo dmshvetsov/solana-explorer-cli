67fff017b95c0a535efa0175adb1c9e564f0a962
dmshvetsov <shvetsovdm@gmail.com>
2025-03-19 14:16:41 +0200

    clean up

--- src/output.rs
+++ src/output.rs
@@ -41,37 +41,8 @@ pub fn print_error<T: Error>(err: T) {
 /// methods required to output a struct from the CLI
 pub trait Output {
     fn struct_name(self: &Self) -> String;
-    // fn struct_name<T>(&self) -> String {
-    //     let type_name = std::any::type_name::<T>().split("::");
-    //     let size = type_name.clone().count();
-    //     let mut type_prefix = type_name.take(size - 1).collect::<Vec<&str>>().join("::");
-    //     if type_prefix.starts_with('&') {
-    //         type_prefix = type_prefix.strip_prefix('&').unwrap().to_string();
-    //     }
-    //     type_prefix
-    // }
 
     fn to_raw_struct(self: &Self) -> String;
 
     fn to_json(self: &Self) -> String;
-    // fn to_json(self: &Self) -> String
-    // where
-    //     Self: Serialize,
-    // {
-    //     serde_json::to_string_pretty(self).unwrap()
-    // }
 }
-
-// impl Debug for dyn Output {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "{:#?}", self)
-//     }
-// }
-
-// impl<T: Output + std::fmt::Debug> std::fmt::Debug for T {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         f.debug_struct("MyTrait")
-//          .field("my_method", &self.my_method())
-//          .finish()
-//     }
-// }
