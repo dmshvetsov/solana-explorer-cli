78f6e134707fcdcfebd33334aef84de69a4c6447
dmshvetsov <shvetsovdm@gmail.com>
2025-08-03 12:56:35 +0200

    add pretty output for main Solana programs

--- src/pretty/public_key.rs
+++ src/pretty/public_key.rs
@@ -3,7 +3,7 @@ use serde;
 use solana_sdk::pubkey::Pubkey;
 use std::fmt;
 
-#[derive(BorshDeserialize)]
+#[derive(BorshDeserialize, Default)]
 pub struct PrettyPublicKey {
     value: Pubkey,
 }
@@ -16,7 +16,28 @@ impl From<Pubkey> for PrettyPublicKey {
 
 impl fmt::Debug for PrettyPublicKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Debug::fmt(&self.value, f)
+        // see Solana Program list here https://github.com/solana-foundation/explorer/blob/master/app/utils/programs.ts
+        match self.value.to_string().as_str() {
+            "11111111111111111111111111111111" => {
+                write!(f, "[Solana System Program] {}", self.value)
+            }
+            "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5mW" => {
+                write!(f, "[Solana Token Program] {}", self.value)
+            }
+            "KeccakSecp256k11111111111111111111111111111" => {
+                write!(f, "[Solana Secp256k1 Program] {}", self.value)
+            }
+            "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL" => {
+                write!(f, "[Solana Associated Token Account Program] {}", self.value)
+            }
+            "CMZYPASGWeTz7RNGHaRJfCq2XQ5pYK6nDvVQxzkH51zb" => {
+                write!(f, "[Magic Eden Launchpad Candy Machine] {}", self.value)
+            }
+            "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d" => {
+                write!(f, "[Metaplex Core Program] {}", self.value)
+            }
+            _ => fmt::Debug::fmt(&self.value, f),
+        }
     }
 }
 
@@ -26,13 +47,5 @@ impl serde::Serialize for PrettyPublicKey {
         S: serde::Serializer,
     {
         serializer.serialize_str(self.value.to_string().as_str())
-    }
-}
-
-impl Default for PrettyPublicKey {
-    fn default() -> Self {
-        PrettyPublicKey {
-            value: Default::default(),
-        }
     }
 }
